3fd28d974700863ecf36417781c6dc46f5aca4e8
EekshithMandhara <eekshith99@gmail.com>
2022-07-07 11:49:29 +0000

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      className="about-image"
+      alt="about"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-paragraph">
+      I Love to Create! Iam a Frontend Developer
+    </p>
+  </div>
+)
+
+export default About
